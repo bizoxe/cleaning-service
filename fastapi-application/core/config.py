9bd19e0001e09c098e70c6cd14e695aaad9cad10
alexander <alfacat187@gmail.com>
2024-12-31 15:07:41 +0300

    add database config + naming convention

--- fastapi-application/core/config.py
+++ fastapi-application/core/config.py
@@ -1,4 +1,11 @@
-from pydantic import BaseModel
+"""
+Main application settings.
+"""
+
+from pydantic import (
+    BaseModel,
+    PostgresDsn,
+)
 from pydantic_settings import (
     BaseSettings,
     SettingsConfigDict,
@@ -24,6 +31,22 @@ class ApiBaseConfig(BaseModel):
     v1: ApiV1Prefix = ApiV1Prefix()
 
 
+class DataBaseConfig(BaseModel):
+    url: PostgresDsn
+    echo: bool = False
+    echo_pool: bool = False
+    pool_size: int = 50
+    max_overflow: int = 10
+
+    naming_convention: dict[str, str] = {
+        "ix": "ix_%(column_0_label)s",
+        "uq": "uq_%(table_name)s_%(column_0_N_name)s",
+        "ck": "ck_%(table_name)s_%(constraint_name)s",
+        "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+        "pk": "pk_%(table_name)s",
+    }
+
+
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(
         env_file=(".env.template", ".env"),
@@ -33,6 +56,7 @@ class Settings(BaseSettings):
     )
     run: RunConfig = RunConfig()
     api: ApiBaseConfig = ApiBaseConfig()
+    db: DataBaseConfig
 
 
 settings = Settings()
