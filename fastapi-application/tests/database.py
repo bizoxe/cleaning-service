fd1ebd687a131583710f67e72da64ed64a4219f1
alexander <alfacat187@gmail.com>
2025-01-11 21:08:51 +0300

    create an interface to connect to the test database

---
+++ fastapi-application/tests/database.py
@@ -0,0 +1,82 @@
+from typing import AsyncIterator
+from contextlib import asynccontextmanager
+
+from sqlalchemy.ext.asyncio import (
+    AsyncEngine,
+    async_sessionmaker,
+    AsyncSession,
+    create_async_engine,
+    AsyncConnection,
+)
+from alembic import (
+    config,
+    command,
+)
+
+
+class DatabaseSessionManager:
+    _ALEMBIC_CFG = config.Config("alembic.ini")
+
+    def __init__(
+        self,
+    ) -> None:
+        self._async_engine: AsyncEngine | None = None
+        self._session_maker: async_sessionmaker[AsyncSession] | None = None
+
+    def init(self, url: str) -> None:
+        self._async_engine = create_async_engine(url=url)
+        self._session_maker = async_sessionmaker(
+            bind=self._async_engine,
+            autocommit=False,
+            expire_on_commit=False,
+        )
+
+    async def close(self) -> None:
+        if self._async_engine is None:
+            raise Exception("DatabaseSessionManager is not initialized")
+        await self._async_engine.dispose()
+        self._async_engine = None
+        self._session_maker = None
+
+    @asynccontextmanager
+    async def connect(self) -> AsyncIterator[AsyncConnection]:
+        if self._async_engine is None:
+            raise Exception("DatabaseSessionManager is not initialized")
+        async with self._async_engine.begin() as connection:
+            try:
+                yield connection
+            except Exception:
+                await connection.rollback()
+                raise
+
+    @asynccontextmanager
+    async def session(self) -> AsyncIterator[AsyncSession]:
+        if self._session_maker is None:
+            raise Exception("DatabaseSessionManager is not initialized")
+        session = self._session_maker()
+        try:
+            yield session
+        except Exception:
+            await session.rollback()
+            raise
+        finally:
+            await session.close()
+
+    @staticmethod
+    def alembic_upgrade(connection, cfg) -> None:
+        cfg.attributes["connection"] = connection
+        command.upgrade(config=cfg, revision="head")
+
+    @staticmethod
+    def alembic_downgrade(connection, cfg) -> None:
+        cfg.attributes["connection"] = connection
+        command.downgrade(config=cfg, revision="base")
+
+    async def create_all(self, connection: AsyncConnection) -> None:
+        await connection.run_sync(self.alembic_upgrade, self._ALEMBIC_CFG)
+
+    async def drop_all(self, connection: AsyncConnection) -> None:
+        await connection.run_sync(self.alembic_downgrade, self._ALEMBIC_CFG)
+
+
+session_manager = DatabaseSessionManager()
