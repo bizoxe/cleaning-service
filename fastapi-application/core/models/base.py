88f1a87836ec3d80ee7a5ed7d4c4397f1906e8de
alexander <alfacat187@gmail.com>
2025-01-12 20:51:22 +0300

    add datetime columns in to base models + converting table fields to dictionary

--- fastapi-application/core/models/base.py
+++ fastapi-application/core/models/base.py
@@ -1,8 +1,18 @@
+from typing import Any
+from datetime import datetime
+
 from sqlalchemy.orm import (
     DeclarativeBase,
     declared_attr,
+    Mapped,
+    mapped_column,
 )
-from sqlalchemy import MetaData
+from sqlalchemy import (
+    MetaData,
+    DateTime,
+    func,
+)
+
 
 from core.config import settings
 from utils import camel_case_to_snake_case
@@ -14,7 +24,20 @@ class Base(DeclarativeBase):
     metadata = MetaData(
         naming_convention=settings.db.naming_convention,
     )
+    id: Any
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        server_default=func.now(),
+    )
+    updated_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        server_default=func.now(),
+        onupdate=func.now(),
+    )
 
     @declared_attr.directive
     def __tablename__(cls) -> str:
         return f"{camel_case_to_snake_case(cls.__name__)}s"
+
+    def as_dict(self) -> dict[str, Any]:
+        return {field.name: getattr(self, field.name) for field in self.__table__.c}
