8a03ded59f6d349ddbe1fd1776a3d0ce6a357fc3
alexander <alfacat187@gmail.com>
2025-01-22 12:59:41 +0300

    create jwt_helpers

---
+++ fastapi-application/api/api_v1/users/jwt_helpers.py
@@ -0,0 +1,63 @@
+"""
+This module contains helper functions for issuing
+access and refresh tokens.
+"""
+
+from typing import Any
+from datetime import timedelta
+
+from core.config import settings
+from auth.utils.auth_utils import encode_jwt
+from auth.schemas import UserAuthSchema
+
+
+TOKEN_TYPE_FIELD = "type"
+ACCESS_TOKEN_TYPE = "access"
+REFRESH_TOKEN_TYPE = "refresh"
+
+
+def create_jwt(
+    token_type: str,
+    token_data: dict[str, Any],
+    expire_minutes: int = settings.auth_jwt.access_token_expire_minutes,
+    expire_timedelta: timedelta | None = None,
+) -> str:
+    jwt_payload = {TOKEN_TYPE_FIELD: token_type}
+    jwt_payload.update(token_data)
+
+    return encode_jwt(
+        payload=jwt_payload,
+        expire_minutes=expire_minutes,
+        expire_timedelta=expire_timedelta,
+    )
+
+
+def create_access_token(
+    user: UserAuthSchema,
+    extra: dict[str, Any] | None = None,
+) -> str:
+    jwt_payload = {
+        "sub": str(user.id),
+        "email": user.email,
+        "scopes": user.permissions,
+    }
+    if extra is not None:
+        jwt_payload.update(**extra)
+
+    return create_jwt(
+        token_type=ACCESS_TOKEN_TYPE,
+        token_data=jwt_payload,
+        expire_minutes=settings.auth_jwt.access_token_expire_minutes,
+    )
+
+
+def create_refresh_token(user: UserAuthSchema) -> str:
+    jwt_payload = {"sub": str(user.id)}
+
+    return create_jwt(
+        token_type=REFRESH_TOKEN_TYPE,
+        token_data=jwt_payload,
+        expire_timedelta=timedelta(
+            days=settings.auth_jwt.refresh_token_expire_days,
+        ),
+    )
