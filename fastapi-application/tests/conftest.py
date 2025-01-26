6bb060719e0edfcb03222b1faf27640f75af1ae5
alexander <alfacat187@gmail.com>
2025-01-26 16:02:05 +0300

    tests: add a database connection to conftest using pytest-postgresql

--- fastapi-application/tests/conftest.py
+++ fastapi-application/tests/conftest.py
@@ -1,88 +1,98 @@
 from typing import AsyncGenerator
+import asyncio
 
-from sqlalchemy.ext.asyncio import (
-    create_async_engine,
-    async_sessionmaker,
-    AsyncEngine,
-    AsyncSession,
-)
-from alembic import (
-    command,
-    config,
-)
+import pytest
+from fastapi import FastAPI
 import pytest_asyncio
 from httpx import (
-    ASGITransport,
     AsyncClient,
+    ASGITransport,
 )
 from asgi_lifespan import LifespanManager
+from pytest_postgresql import factories
+from pytest_postgresql.janitor import DatabaseJanitor
 
-from core.config import settings
 from server.create_fastapi_app import create_app
+from tests.database import session_manager
 from core.models import db_helper
+from core.config import settings
 
 
-TEST_DB_URL = str(settings.db.test_db_url)
+class CustomEventLoopPolicy(asyncio.DefaultEventLoopPolicy):
+    pass
 
 
-async_engine: AsyncEngine = create_async_engine(
-    url=TEST_DB_URL,
-    echo=False,
-    echo_pool=False,
+@pytest.fixture(autouse=True)
+def app() -> FastAPI:
+    _app = create_app()
+
+    return _app
+
+
+@pytest_asyncio.fixture(scope="function")
+async def client(app) -> AsyncGenerator[AsyncClient, None]:
+    async with LifespanManager(app):
+        transport = ASGITransport(app=app)
+        async with AsyncClient(
+            transport=transport,
+            base_url="http://test",
+            headers={"Content-Type": "application/json"},
+        ) as client:
+
+            yield client
+
+
+test_db = factories.postgresql_noproc(
+    host=settings.db.postgres_host,
+    port=settings.db.postgres_port,
+    user=settings.db.postgres_user,
+    password=settings.db.postgres_pwd,
+    dbname="test_db",
 )
 
 
-def run_upgrade(connection, cfg):
-    cfg.attributes["connection"] = connection
-    command.upgrade(cfg, "head")
+@pytest.fixture(scope="session", params=(CustomEventLoopPolicy(),))
+def event_loop_policy(request):
+    return request.param
 
 
-def run_downgrade(connection, cfg):
-    cfg.attributes["connection"] = connection
-    command.downgrade(cfg, "base")
+@pytest_asyncio.fixture(scope="module", autouse=True)
+async def connection_test(test_db, event_loop_policy):
+    pg_host = test_db.host
+    pg_port = test_db.port
+    pg_user = test_db.user
+    pg_password = test_db.password
+    pg_db = test_db.dbname
+    version = test_db.version
+    with DatabaseJanitor(
+        user=pg_user,
+        host=pg_host,
+        port=pg_port,
+        dbname=pg_db,
+        password=pg_password,
+        version=version,
+    ):
+        connection_str = (
+            f"postgresql+psycopg://{pg_user}:{pg_password}@{pg_host}:{pg_port}/{pg_db}"
+        )
+
+        session_manager.init(connection_str)
+
+        yield
+        await session_manager.close()
 
 
 @pytest_asyncio.fixture(scope="function", autouse=True)
-async def apply_migrations() -> None:
-    """
-    Runs migrations before testing.
-    """
-    alembic_cfg = config.Config("alembic.ini")
-    async with async_engine.begin() as conn:
-        await conn.run_sync(run_downgrade, alembic_cfg)
-        await conn.run_sync(run_upgrade, alembic_cfg)
+async def create_tables(connection_test) -> None:
+    async with session_manager.connect() as connection:
+        await session_manager.drop_all(connection)
+        await session_manager.create_all(connection)
 
 
-async def override_session_getter() -> AsyncGenerator[AsyncSession, None]:
-    """
-    Is used to connect to the test database and get the session.
-    Returns:
-            The database session.
-    """
-    session_factory: async_sessionmaker[AsyncSession] = async_sessionmaker(
-        bind=async_engine,
-        autoflush=False,
-        autocommit=False,
-        expire_on_commit=False,
-    )
-    async with session_factory() as session:
-        yield session
+@pytest_asyncio.fixture(scope="function", autouse=True)
+async def session_override(app, connection_test):
+    async def get_db_override():
+        async with session_manager.session() as session:
+            yield session
 
-    await async_engine.dispose()
-
-
-@pytest_asyncio.fixture(loop_scope="module", scope="module")
-async def test_app():
-    app = create_app()
-    app.dependency_overrides[db_helper.session_getter] = override_session_getter
-
-    return app
-
-
-@pytest_asyncio.fixture(loop_scope="module", scope="module")
-async def client(test_app) -> AsyncGenerator[AsyncClient, None]:
-    async with LifespanManager(test_app):
-        transport = ASGITransport(app=test_app)
-        async with AsyncClient(transport=transport, base_url="http://test") as client:
-
-            yield client
+    app.dependency_overrides[db_helper.session_getter] = get_db_override
